bd52c1cdef2dbf9086f454a2629a608a6814e71f
pcvbjr <pcvbjr@gmail.com>
2022-03-08 20:21:02 -0500

    Initial Commit

---
+++ House Cleaning/House_CleaningApp.swift
@@ -0,0 +1,17 @@
+//
+//  House_CleaningApp.swift
+//  House Cleaning
+//
+//  Created by Chris Van Buren on 3/8/22.
+//
+
+import SwiftUI
+
+@main
+struct House_CleaningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
