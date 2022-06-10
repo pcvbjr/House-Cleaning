388d08c2b3584757c581fb199afaea08029bd5fe
kmccarthy13 <101230843+kmccarthy13@users.noreply.github.com>
2022-06-09 23:18:12 -0400

    Update ContentView.swift

--- House Cleaning/ContentView.swift
+++ House Cleaning/ContentView.swift
@@ -7,54 +7,39 @@
 
 import SwiftUI
 
+func getDocumentsDirectory() -> URL {
+    // find all possible documents directories for this user
+    let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+
+    // just send back the first one, which ought to be the only one
+    return paths[0]
+}
 
 struct ContentView: View {
-    // set the t/f value of your CheckboxView view
-    @State var checked = true
     var body: some View {
         NavigationView{
-            VStack(alignment: .center) {
-                Text("CleanWiz")
-                    .fontWeight(.light)
-                    .padding()
-                    .font(.title3)
-                    .foregroundColor(.blue)
-                Text("Hi, neat freak.")
-                    .fontWeight(.bold)
-                    .padding()
-                    .font(.title)
-                    .frame(width: 400, height: 100, alignment: .leading)
-                Text(Date(), style: .date)
-                    .font(.title3)
-                    .padding()
-                    .frame(width: 400, height: 100, alignment: .leading)
-                Text("Today's Tasks")
-                    .font(.headline)
-                    .padding()
-                    .frame(width: 400, height: 80, alignment: .leading)
-                
-                // tasks dynamically populate within frame here
-                
-                HStack {
-                    CheckboxView(checked: $checked)
-                    Text("Element that requires checkmark!")
+            NavigationTabsView()
+                .navigationTitle("CleanWiz")
+                .navigationBarTitleDisplayMode(.inline)
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                              Button(action: {
+                                print("Settings")
+                              }) {
+                                Label("Send", systemImage: "gearshape.fill")
+                              }
+                            }
                 }
-                .padding()
-                Divider()
-                .padding()
-                DetailViewCTA()
-                
-                
-                Spacer()
-            }
         }
-		
+        
     }
+        
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-			.environment(\.locale, Locale(identifier: "us"))
+            .environment(\.locale, Locale(identifier: "us"))
     }
 }
