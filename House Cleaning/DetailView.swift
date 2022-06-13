fcfede0e94d9b2521223745eb1cf6e11d739ec96
kmccarthy13 <101230843+kmccarthy13@users.noreply.github.com>
2022-06-12 23:34:50 -0400

    Began updating View
    Added in functionality to build new arrays based on status. The goal is that this Array will be used for the Expandable List element.

--- House Cleaning/DetailView.swift
+++ House Cleaning/DetailView.swift
@@ -2,14 +2,25 @@
 //  DetailView.swift
 //  House Cleaning
 //
-//  Created by Kaitlyn McCarthy on 3/12/22.
+//  Created by Chris Van Buren on 3/12/22.
 //
 
 import SwiftUI
 
 struct DetailView: View {
     var body: some View {
-        Text("DetailView page")
+        //Text("DetailView page")
+        var dueOtherArray = buildArray(status: "Due: other")
+        var length = dueOtherArray.count
+//        VStack {
+//            ForEach(dueOtherArray) { user in
+//                    Text(user.name)
+//            }
+//        }
+        List(dueOtherArray){ user in
+            Text(user.name)
+            
+        }
     }
 }
 
