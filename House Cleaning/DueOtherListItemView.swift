7f2c45d360c44d8a1ead7505ce30437cdc431e09
kmccarthy13 <101230843+kmccarthy13@users.noreply.github.com>
2022-06-12 23:44:21 -0400

    Updated styles

--- House Cleaning/DueOtherListItemView.swift
+++ House Cleaning/DueOtherListItemView.swift
@@ -17,29 +17,41 @@ struct DueOtherListItemView: View {
     
     var body: some View {
         ZStack{
-            Color.gray.padding([.leading, .trailing], -20)
+            Color(.white).padding([.leading, .trailing], -20)
             HStack{
                 CheckboxView(checked: $checked, id: id, duration: duration, datas: datas)
             //}
 
                 VStack(alignment: .leading) {
-                    Text(name)
-                        .font(.title)
-                        .fontWeight(.heavy)
-                        .foregroundColor(Color.black)
-                    HStack{
-                        Text(duration)
+                    HStack {
+                        Text(name)
                             .font(.title3)
-                            .foregroundColor(Color.yellow)
+                            .foregroundColor(Color.black)
+                            .fontWeight(.bold)
+                        Spacer()
+                        Text("DUE LATER")
+                            .font(.caption)
+                            .fontWeight(.bold)
+                            .padding(4)
+                            .background(Color("CustomGrayColor"))
+                            .foregroundColor(Color("FontColor"))
+                            .border(Color("CustomGrayColor"), width: 2)
+                    }
+                    Spacer()
+                    HStack{
+                        Text(duration + " days")
+                            .font(.body)
+                            .foregroundColor(Color.black)
                         Spacer()
                         Text(date)
-                            .font(.title3)
+                            .font(.body)
                     }
                 }
             }
         }
     }
 }
+
 
 //struct DueOtherListItemView_Previews: PreviewProvider {
 //    static var previews: some View {
