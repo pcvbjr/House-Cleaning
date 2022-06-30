c6a70464f63a7bb7961bc457f91cf94a1b9c6acf
kmccarthy13 <101230843+kmccarthy13@users.noreply.github.com>
2022-06-29 22:20:52 -0400

    Update ContentView.swift

--- House Cleaning/ContentView.swift
+++ House Cleaning/ContentView.swift
@@ -8,38 +8,38 @@
 import SwiftUI
 
 func getDocumentsDirectory() -> URL {
-	// find all possible documents directories for this user
-	let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+    // find all possible documents directories for this user
+    let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
 
-	// just send back the first one, which ought to be the only one
-	return paths[0]
+    // just send back the first one, which ought to be the only one
+    return paths[0]
 }
 
 struct ContentView: View {
     var body: some View {
-		NavigationView{
-			NavigationTabsView()
-				.navigationTitle("CleanWiz")
-				.navigationBarTitleDisplayMode(.inline)
-				.toolbar {
-					ToolbarItem(placement: .navigationBarTrailing) {
-							  Button(action: {
-								print("Settings")
-							  }) {
-								Label("Send", systemImage: "gearshape.fill")
-							  }
-							}
-				}
-		}
-		
-	}
-		
+        NavigationView{
+            NavigationTabsView()
+                .navigationTitle("CleanWiz")
+                .navigationBarTitleDisplayMode(.inline)
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                              Button(action: {
+                                print("Settings")
+                              }) {
+                                Label("Send", systemImage: "gearshape.fill")
+                              }
+                            }
+                }
+        }
+        
+    }
+        
 }
 
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-			.environment(\.locale, Locale(identifier: "us"))
+            .environment(\.locale, Locale(identifier: "us"))
     }
 }
